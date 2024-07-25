7cfed4fe4ee94945033cfa12e8a14d48c89032b1
Fourfold <fourfold164@gmail.com>
2024-07-25 16:31:19 +0300

    Modify main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 int main() {
-    Person bassam = Person("Bassam");
+    Person eyad = Person("Eyad");
+    Person bassam = Person("Bassam", "", &eyad);
     Person firas = Person("Firas", "30/04/2002", &bassam);
     firas.set_occupation("Engineer");
 
@@ -12,6 +13,8 @@ int main() {
     else
         cout << "Bassam does not have any children.\n";
     
-    cout << "Firas's occupation is: " << firas.get_occupation();
+    cout << "Firas's occupation is: " << firas.get_occupation() << endl;
+
+    cout << "Firas's grandfather is: " << firas.get_father()->get_father()->get_name() << endl;
     return 0;
 }
